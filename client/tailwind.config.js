0b6090be370f1c861edf36f7ebbfc6b98844f1d3
Rebecca Crecelius <info@rebeccacrecelius.com>
2024-07-28 15:29:52 -0500

    First commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightPrimary: '#CEC3C8',
+      }
+    },
+  },
+  plugins: [],
+}
+
